1986c3620963e8efe81eb6147a855d7efe5fbac7
Suntorio <suntorio80@gmail.com>
2023-10-20 12:44:13 -0700

    updates

--- Ansible_homework/main.tf
+++ Ansible_homework/main.tf
@@ -36,13 +36,13 @@ resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
     from_port   = 0
-    to_port     = 65000
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
-    to_port     = 65000
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
