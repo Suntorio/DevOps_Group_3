2242a89badba36ae392c99a443e4ea9f2d03c469
Suntorio <suntorio80@gmail.com>
2023-11-01 21:06:12 -0700

    updates

--- terraform_ansible_jenkins_setup/instances.tf
+++ terraform_ansible_jenkins_setup/instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "jenkins" {
   ami                    = "ami-053b0d53c279acc90" # Ubuntu server 22.04
   instance_type          = "t4g.small" #"t2.micro" # Do not forget to turn off the instance after the test (56$ per month)
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  key_name               = "lesson_7_ansible"
+  key_name               = "jenkins-ansible"
   iam_instance_profile = aws_iam_instance_profile.jenkins_profile.name
 
   tags = {
