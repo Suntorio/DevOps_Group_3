3e2f99c5e453f1120cdfdfdd0252330f4499f9f8
Suntorio <suntorio80@gmail.com>
2023-11-08 18:20:55 -0800

    change the path

--- DevOps_jenkins_tf_ans_node_js_Setup/jenkins_pipeline/all_atonce.groovy
+++ DevOps_jenkins_tf_ans_node_js_Setup/jenkins_pipeline/all_atonce.groovy
@@ -33,7 +33,7 @@ pipeline {
         }
         stage('Terraform Plan') {
             steps {
-                dir('terraform_ansible_generic_instace_setup_template') {
+                dir('DevOps_jenkins_tf_ans_node_js_Setup/terraform_ansible_generic_instace_setup_template') {
                     sh '''
                     echo "yes" | terraform init
                     terraform plan -out=terraform.tfplan
@@ -63,14 +63,14 @@ pipeline {
         }
         stage('Terraform Apply') {
             steps {
-                dir('terraform_ansible_generic_instace_setup_template') {
+                dir('DevOps_jenkins_tf_ans_node_js_Setup/terraform_ansible_generic_instace_setup_template') {
                     sh 'terraform apply terraform.tfplan'
                 }
             }
         }
         stage('Get Terraform Outputs') {
             steps {
-                dir('terraform_ansible_generic_instace_setup_template') {
+                dir('DevOps_jenkins_tf_ans_node_js_Setup/terraform_ansible_generic_instace_setup_template') {
                     sh 'terraform output web-address-nodejs > ./ansible/instance_ip.txt'
                 }
             }
@@ -87,7 +87,7 @@ pipeline {
         stage('Run Ansible for the battlkships app') {
             steps {
                 withCredentials([sshUserPrivateKey(credentialsId: 'access_for_new_node_js_app', keyFileVariable: 'SSH_KEY')]) {
-                    dir('terraform_ansible_generic_instace_setup_template/ansible'){
+                    dir('DevOps_jenkins_tf_ans_node_js_Setup/terraform_ansible_generic_instace_setup_template/ansible'){
                         sh '''
                         sleep 120
                         ansible-playbook -i instance_ip.txt playbook_nodejs_battleships.yaml -u ubuntu --private-key=$SSH_KEY -e 'ansible_ssh_common_args="-o StrictHostKeyChecking=no"'
@@ -100,7 +100,7 @@ pipeline {
         stage('Run Ansible for the dadjokes app') {
             steps {
                 withCredentials([sshUserPrivateKey(credentialsId: 'access_for_new_node_js_app', keyFileVariable: 'SSH_KEY')]) {
-                    dir('terraform_ansible_generic_instace_setup_template/ansible'){
+                    dir('DevOps_jenkins_tf_ans_node_js_Setup/terraform_ansible_generic_instace_setup_template/ansible'){
                         sh '''
                         ansible-playbook -i instance_ip.txt playbook_nodejs_playbook_nodejs_dadjokes.yaml -u ubuntu --private-key=$SSH_KEY -e 'ansible_ssh_common_args="-o StrictHostKeyChecking=no"'
                         '''
