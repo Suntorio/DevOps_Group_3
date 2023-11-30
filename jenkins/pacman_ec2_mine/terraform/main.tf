380788f7680cc628469ff98e7ce846ab1f640029
Suntorio <suntorio80@gmail.com>
2023-11-29 21:47:22 -0800

    update

--- jenkins/pacman_ec2_mine/terraform/main.tf
+++ jenkins/pacman_ec2_mine/terraform/main.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 1.3"
   backend "s3" {
     bucket = "terraform-state-test-my-cloud-aleks"
-    key    = "nodejsapp_example/terraform.tfstate"
+    key    = "pacman/terraform.tfstate"
     region = "us-east-1"
   }
 }
