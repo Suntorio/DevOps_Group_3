985d5137ce589e6248b0830e546d2bbe9b1aebf3
Suntorio <suntorio80@gmail.com>
2023-11-29 22:10:50 -0800

    upd

--- jenkins/pacman_ec2_mine/terraform/instances.tf
+++ jenkins/pacman_ec2_mine/terraform/instances.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "nodejs-app" {
+resource "aws_instance" "pacman-nodejs-app" {
   ami                    = "ami-053b0d53c279acc90" # Ubuntu server 22.04
   instance_type          = "t3.nano" # Do not forget to turn off the instance after the test is complete ( around $30 monthly)
   vpc_security_group_ids = [aws_security_group.web-sg.id]
