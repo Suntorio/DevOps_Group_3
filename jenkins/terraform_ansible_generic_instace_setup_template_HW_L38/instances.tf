36ffc3b6aa984f73d1e84be367c40d5851955b0b
Suntorio <suntorio80@gmail.com>
2023-12-13 18:11:11 -0800

    updates

--- jenkins/terraform_ansible_generic_instace_setup_template_HW_L38/instances.tf
+++ jenkins/terraform_ansible_generic_instace_setup_template_HW_L38/instances.tf
@@ -1,29 +1,29 @@
-resource "aws_instance" "nodejs-app-01" {
+resource "aws_instance" "k3s-master_node" {
   ami                    = "ami-0fc5d935ebf8bc3bc"  #"ami-053b0d53c279acc90" # Ubuntu server 22.04
   instance_type          = "c6a.large" #"t2.micro" # Do not forget to turn off the instance after the test is complete (around $30 monthly)
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   key_name               = "jenkins-ansible"
   tags = {
-    Name = "Node_JS_app_L38_01"
+    Name = "K3S-Master_Node"
   }
 }
 
-resource "aws_instance" "nodejs-app-02" {
+resource "aws_instance" "k3s-worker_01" {
   ami                    = "ami-0fc5d935ebf8bc3bc"  #"ami-053b0d53c279acc90" # Ubuntu server 22.04
   instance_type          = "c6a.large" #"t2.micro" # Do not forget to turn off the instance after the test is complete (around $30 monthly)
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   key_name               = "jenkins-ansible"
   tags = {
-    Name = "Node_JS_app_L38_02"
+    Name = "K3S-Worker_01"
   }
 }
 
-resource "aws_instance" "nodejs-app-03" {
+resource "aws_instance" "k3s-worker_02" {
   ami                    = "ami-0fc5d935ebf8bc3bc"  #"ami-053b0d53c279acc90" # Ubuntu server 22.04
   instance_type          = "c6a.large" #"t2.micro" # Do not forget to turn off the instance after the test is complete (around $30 monthly)
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   key_name               = "jenkins-ansible"
   tags = {
-    Name = "Node_JS_app_L38_03"
+    Name = "K3S-Worker_02"
   }
 }
