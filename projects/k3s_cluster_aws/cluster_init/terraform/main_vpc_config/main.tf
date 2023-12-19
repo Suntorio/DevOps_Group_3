f9f647ddab6ac868e273b3aa74159c5b8694c749
Suntorio <suntorio80@gmail.com>
2023-12-19 14:11:44 -0800

    updates

--- projects/k3s_cluster_aws/cluster_init/terraform/main_vpc_config/main.tf
+++ projects/k3s_cluster_aws/cluster_init/terraform/main_vpc_config/main.tf
@@ -10,7 +10,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "terraform-state-oleksii"
+    bucket = "terraform-state-test-my-cloud-aleks"
     key    = "k3s_main_vpc/infra_setup.tfstate"
     region = "us-east-1"
   }
