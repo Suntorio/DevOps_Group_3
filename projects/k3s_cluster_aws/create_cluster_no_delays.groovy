f4013c25de69a4f67c5607cf436a5a2ec673e2e7
Suntorio <suntorio80@gmail.com>
2024-01-25 20:19:18 -0800

    -S

--- projects/k3s_cluster_aws/create_cluster_no_delays.groovy
+++ projects/k3s_cluster_aws/create_cluster_no_delays.groovy
@@ -30,7 +30,7 @@ pipeline {
                 sh '''
                 sudo apt-get update
                 sudo apt-get install -y jq apt-transport-https ca-certificates curl
-                sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+                sudo curl -S https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
                 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
                 sudo apt-get update
                 sudo apt-get install -y kubectl ansible
